0e3d7123ee2b4b0a5f59c65718b02574f5b2ad3b
cdcalderon <cdcalderon@gmail.com>
2024-01-28 10:33:24 -0600

    refactor to use environment variables

--- src/app/shared/relevanceai.service.ts
+++ src/app/shared/relevanceai.service.ts
@@ -2,31 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RelevanceAIResponse } from '../models/relevance-ai-response';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RelevanceAIService {
-relevanceAiUrlBase = 'https://api-bcbe5a.stack.tryrelevance.com/latest/studios/7557f99d-b7a4-4e28-b181-c73de1598a2c/trigger_limited'
-transcriptApiUrl = 'http://localhost:3000/transcript'; // Add your API base URL here
+  relevanceAiUrlBase = environment.relevanceAiUrlBase;
+  transcriptApiUrl = environment.transcriptApiUrl;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
-  }
-
-  triggerRelevanceAI(transcript: string, user_prompt: string): Observable<RelevanceAIResponse> {
+  triggerRelevanceAI(
+    transcript: string,
+    user_prompt: string
+  ): Observable<RelevanceAIResponse> {
     const payload = {
       params: {
         transcript: transcript,
-        user_prompt: user_prompt
+        user_prompt: user_prompt,
       },
-      project: 'ed594377ff96-49bf-b2ab-5a728736595a'
+      project: 'ed594377ff96-49bf-b2ab-5a728736595a',
     };
 
-    return this.http.post<RelevanceAIResponse>(this.relevanceAiUrlBase, payload);
+    return this.http.post<RelevanceAIResponse>(
+      this.relevanceAiUrlBase,
+      payload
+    );
   }
 
   fetchTranscript(videoId: string): Observable<string> {
-    return this.http.get(`${this.transcriptApiUrl}/${videoId}`, { responseType: 'text' });
+    return this.http.get(`${this.transcriptApiUrl}/${videoId}`, {
+      responseType: 'text',
+    });
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  relevanceAiUrlBase: 'https://api-bcbe5a.stack.tryrelevance.com/latest/studios/7557f99d-b7a4-4e28-b181-c73de1598a2c/trigger_limited',
+  transcriptApiUrl: 'http://localhost:3000/transcript',
+};
