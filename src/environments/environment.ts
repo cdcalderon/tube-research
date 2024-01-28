f55292b7bcfb7133dff29848c1ccbacfdf21eabd
cdcalderon <cdcalderon@gmail.com>
2024-01-28 11:53:22 -0600

    update build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: false,
-  relevanceAiUrlBase: 'https://api-bcbe5a.stack.tryrelevance.com/latest/studios/7557f99d-b7a4-4e28-b181-c73de1598a2c/trigger_limited',
-  transcriptApiUrl: 'http://localhost:3000/transcript',
+  production: true,
+  relevanceAiUrlBase:
+    'https://api-bcbe5a.stack.tryrelevance.com/latest/studios/7557f99d-b7a4-4e28-b181-c73de1598a2c/trigger_limited',
+  transcriptApiUrl:
+    'https://youtubetranscriptgetter-production.up.railway.app/transcript',
 };
