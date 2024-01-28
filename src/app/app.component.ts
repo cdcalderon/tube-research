10f87dd8f95f7ca772d6196fceb37c9c8138ae84
cdcalderon <cdcalderon@gmail.com>
2024-01-27 20:45:33 -0600

    update ui so it read messages from component ad message array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RelevanceAIService } from './shared/relevanceai.service';
 import { FormsModule } from '@angular/forms';
+import { ChatMessage } from './models/chat-message';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [CommonModule,RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,7 +16,11 @@ export class AppComponent {
   title = 'tube-research';
   userPrompt: string = ''; // Bind this to the input field
   mockTranscript: string = 'Your mock video transcript here...'; // Replace with your mock transcript
-
+  messages: ChatMessage[] = [
+    { content: "Message from User 1", sender: "User" },
+    { content: "Message from AI 1", sender: "AI" },
+    { content: "Message from User 2", sender: "User" },
+  ];
 
   constructor(private relevanceAIService: RelevanceAIService){
 
